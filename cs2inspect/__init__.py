8d1a07a0d83f7232227b8f4a71e72710d5e20071
lukas <m@hler.eu>
2026-04-22 18:22:41 +0200

    Update project metadata, typing markers, and remove redundant manifest

--- cs2inspect/__init__.py
+++ cs2inspect/__init__.py
@@ -1,9 +1,16 @@
-from cs2inspect._create import gen, link, link_console, link_masked, link_unmasked
-from cs2inspect._parse import UnsupportedItemError, parse, unlink
-from cs2inspect._proto import Builder
-from cs2inspect._schema import ItemSchema, download_schema, load_schema, load_schema_path
-from cs2inspect._util_hex import from_hex, to_hex
-from cs2inspect._util_link import is_link_quoted, is_link_valid, link_type, quote_link, unquote_link
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version("cs2inspect")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "unknown"
+
+from ._create import gen, link, link_console, link_masked, link_unmasked
+from ._parse import UnsupportedItemError, parse, unlink
+from ._proto import Builder
+from ._schema import ItemSchema, download_schema, load_schema, load_schema_path
+from ._util_hex import from_hex, to_hex
+from ._util_link import is_link_quoted, is_link_valid, link_type, quote_link, unquote_link
 
 __all__ = [
     # Core API
@@ -32,7 +39,3 @@ __all__ = [
     "from_hex",
     "to_hex",
 ]
-
-
-if __name__ == "__main__":
-    exit(1)
